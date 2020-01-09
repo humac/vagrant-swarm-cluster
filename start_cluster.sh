3429430ab49f2891283394de55ff5688a89a4c7b
Huy Nguyen <huy.nguyen.d@gmail.com>
2020-01-08 19:14:40 -0500

    Update start_cluster.sh

--- start_cluster.sh
+++ start_cluster.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-echo "Starting manager..."
+echo "Starting manager node1..."
 docker -H tcp://10.0.7.10:2375 swarm init --advertise-addr 10.0.7.10
 
 echo "Retrieve manager token..."
 TOKEN=$(docker -H tcp://10.0.7.10:2375 swarm join-token -q worker)
 
-echo "Starting node..."
+echo "Starting worker node2..."
 docker -H tcp://10.0.7.11:2375 swarm join --token "${TOKEN}" 10.0.7.10:2377
+
+echo "Starting worker node3..."
+docker -H tcp://10.0.7.12:2375 swarm join --token "${TOKEN}" 10.0.7.10:2377
 
 docker -H tcp://10.0.7.10:2375 info
